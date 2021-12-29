0643992db485810e38a8ae3138e4865cd16e440f
Ahmed <arizkcisco@gmail.com>
2021-12-30 01:20:12 +0200

    Prettier added

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import express, {Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from 'express';
 import helmet from 'helmet';
 
 const app: Application = express();
 app.use(helmet());
 
 const port: number = 5000;
-const host: string = 'localhost'
+const host: string = 'localhost';
 
-app.listen(port, host, () => console.log('Server ' + host + ' is listening on port ' + port))
+app.listen(port, host, () =>
+    console.log('Server ' + host + ' is listening on port ' + port)
+);
 
-app.get('/', (req:Request, res:Response) => {
-
-  res.status(200).send("Server is running successfully!")
-
-})
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Server is running successfully!');
+});
 
 export default app;
